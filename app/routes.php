6a3c150af0340c7382cb2fa58dc78f01c93612b2
Corentin Dodon <corentin.dodon@gmail.com>
2015-10-15 16:55:43 +0200

    routes
    oui

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,19 @@
 <?php
 
 $app->get('/', function () use ($app) {
-    return $app->redirect('index.php/login');
+	$url=$app["url_generator"]->generate("index");
+	$test=substr($url,0,9)."index.php/".substr($url,9);
+    return $app->redirect($test);
+});
+
+$app->get('/index.php/admin/accueil', function () use ($app) {
+	$url=$app["url_generator"]->generate("index");
+    return $app->redirect($url);
 });
 
 $app->get('/login', "GSB\Controller\LoginController::loginAction")
 	->bind('login');
-$app->get('/admin/a', "GSB\Controller\LoginController::indexAction")
+$app->get('/admin/accueil', "GSB\Controller\LoginController::indexAction")
 	->bind('index');
 $app->get('/admin/praticien', "GSB\Controller\PraticienController::indexAction")
 	->bind('listPraticien');
